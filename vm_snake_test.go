380cd77c81bae2244ecaacc7678ae5eaf7574cc0
Joshua T Corbin <jcorbin@wunjo.org>
2017-08-10 07:58:55 -0700

    snake: add entry point jump

--- vm_snake_test.go
+++ vm_snake_test.go
@@ -24,7 +24,8 @@ func Test_snakeCube(t *testing.T) {
 		M := len(labels)
 
 		code := []interface{}{
-			0x40, // stack size
+			0x40,                   // stack size
+			":chooseStart", "jump", // TODO entry point
 
 			//// definitions and setup
 
