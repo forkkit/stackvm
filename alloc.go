037453992f8d4385a7a44fc8593d485f0eee62c4
Joshua T Corbin <jcorbin@wunjo.org>
2017-08-16 08:23:39 -0700

    Drop pool allocators

--- alloc.go
+++ alloc.go
@@ -1,7 +1,5 @@
 package stackvm
 
-import "sync"
-
 type machAllocator interface {
 	AllocMach() (*Mach, error)
 	FreeMach(*Mach)
@@ -11,44 +9,6 @@ type pageAllocator interface {
 	AllocPage() *page
 	FreePage(*page)
 }
-
-var (
-	machPool = sync.Pool{New: func() interface{} { return &Mach{} }}
-	pagePool sync.Pool
-)
-
-type _machPoolAllocator struct{}
-
-func (mpa _machPoolAllocator) AllocMach() (*Mach, error) {
-	return machPool.Get().(*Mach), nil
-}
-
-func (mpa _machPoolAllocator) FreeMach(m *Mach) {
-	machPool.Put(m)
-}
-
-type _pagePoolAllocator struct{}
-
-func (ppa _pagePoolAllocator) AllocPage() *page {
-	if v := pagePool.Get(); v != nil {
-		if pg, ok := v.(*page); ok {
-			for i := range pg.d {
-				pg.d[i] = 0
-			}
-			return pg
-		}
-	}
-	return &page{r: 0}
-}
-
-func (ppa _pagePoolAllocator) FreePage(pg *page) {
-	pagePool.Put(pg)
-}
-
-var (
-	machPoolAllocator machAllocator = _machPoolAllocator{}
-	pagePoolAllocator pageAllocator = _pagePoolAllocator{}
-)
 
 func makeMachFreeList(n int) *machFreeList { return &machFreeList{make([]*Mach, 0, n)} }
 func makePageFreeList(n int) *pageFreeList { return &pageFreeList{make([]*page, 0, n)} }
