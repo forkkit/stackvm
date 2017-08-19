3c7a40ac92231d78278ffaa17763815f8b2095f0
Joshua T Corbin <jcorbin@wunjo.org>
2017-08-19 10:24:19 -0700

    x/assemble: add .queueSize directive

--- x/assemble.go
+++ x/assemble.go
@@ -118,6 +118,18 @@ func (asm *assembler) scan() error {
 	return asm.buildRefs()
 }
 
+func (asm *assembler) handleQueueSize() error {
+	n, err := asm.expectInt("queueSize int")
+	if err != nil {
+		return err
+	}
+	if n < 0 {
+		return fmt.Errorf("invalid .queueSize %v, must be non-negative", n)
+	}
+	asm.opts.QueueSize = uint32(n)
+	return nil
+}
+
 func (asm *assembler) handleMaxOps() error {
 	n, err := asm.expectInt("maxOps int")
 	if err != nil {
@@ -200,6 +212,8 @@ func (asm *assembler) handleDirective(name string) error {
 		return asm.handleEntry()
 	case "stackSize":
 		return asm.handleStackSize()
+	case "queueSize":
+		return asm.handleQueueSize()
 	case "maxOps":
 		return asm.handleMaxOps()
 	case "data":
